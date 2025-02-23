ed60c531f746d7a13efe24de41485e73861bfb8d
Israt Jahan <eraisrat022@gmail.com>
2025-02-23 23:11:47 +0600

    Email setup

--- event_management/settings.py
+++ event_management/settings.py
@@ -152,7 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS',cast=bool)
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS',cast=bool)
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False").lower() in ["true", "1", "yes"]
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
